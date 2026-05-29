a633b7a50500650f595f3abad271f7d725f3be7f
@wyyyywwwww <2171498913@qq.com>
2026-05-29 17:34:34 +0800

    进程

--- 进程/main.c
+++ 进程/main.c
@@ -78,24 +78,35 @@
 //
 
 
-int main() 
+//int main() 
+//{
+//    int val = 100;
+//    pid_t pid = fork();
+//        
+//        if (pid == 0) 
+//        {
+//                // 子进程修改val的值
+//                     int  val = 200;
+//                         printf("子进程：val = %d，地址：%p\n", val, &val);
+//        }
+//                         else 
+//                         {
+//                                  
+//                           sleep(1); // 等待子进程先执行
+//                          printf("父进程：val = %d，地址：%p\n", val, &val);
+//                                           
+//                         }
+//                                               
+//             return 0;
+//}
+
+int main()
 {
-    int val = 100;
-    pid_t pid = fork();
-        
-        if (pid == 0) 
-        {
-                // 子进程修改val的值
-                     int  val = 200;
-                         printf("子进程：val = %d，地址：%p\n", val, &val);
-        }
-                         else 
-                         {
-                                  
-                           sleep(1); // 等待子进程先执行
-                          printf("父进程：val = %d，地址：%p\n", val, &val);
-                                           
-                         }
-                                               
-             return 0;
+
+  while(1)
+  {
+    printf("我是一个进程,%d\n",getpid());
+    sleep(1);
+  }
+  return 0;
 }
